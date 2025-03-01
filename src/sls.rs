7eec20b67bc628c16c2ac4c37bc1e3ff139f8293
Lukas <oss.heindl+git@protonmail.com>
2025-03-02 00:10:12 +0100

    make sls more robust

--- src/sls.rs
+++ src/sls.rs
@@ -6,29 +6,35 @@ where
 {
     let mut distribution: BTreeMap<T, u64> = BTreeMap::new();
 
-    // TODO use https://docs.rs/fixed/latest/fixed/ here?
     let mut zut_div: f64 = xs.iter().map(|x| x.1).sum::<f64>() / total as f64;
-    println!(
-        "sls start: {zut_div} | {total} | {}",
-        xs.iter().map(|x| x.1).sum::<f64>()
-    );
+    // println!(
+    //     "sls start: {zut_div} | {total} | {}",
+    //     xs.iter().map(|x| x.1).sum::<f64>()
+    // );
 
+    // find a working divisor
     let mut s = distribution.iter().map(|x| x.1).sum();
+    let mut diff = 1.0;
+    let mut last = (0.0, 0.0);
     while s != total {
         if s < total {
-            zut_div -= 1.0;
+            zut_div -= diff;
         } else {
-            zut_div += 1.0;
+            zut_div += diff;
         }
+        if (last.1 - zut_div).abs() < 10e-10_f64 {
+            diff *= 0.5;
+        }
+        last = (zut_div, last.0);
 
         for (id, x) in xs.iter() {
-            // println!("sls: {id} -> {}", *x/zut_div);
             distribution.insert(id.clone(), (*x / zut_div).round() as u64);
         }
 
         s = distribution.iter().map(|x| x.1).sum::<u64>();
         // println!();
     }
-    println!("sls res: {zut_div}");
+
+    // println!("sls res: {zut_div}");
     distribution
 }
